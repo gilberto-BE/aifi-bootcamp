a228146647116254922bae055e06836bc916ddf9
gilberto-BE <g.batres.estrada@gmail.com>
2022-02-22 21:30:14 +0100

    move code to src folder

--- archive/preprocess.py
+++ archive/preprocess.py
@@ -15,11 +15,11 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import tensorflow as tf
+# import tensorflow as tf
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, QuantileTransformer
 from sklearn.model_selection import train_test_split
-tf.random.set_seed(13)
+# tf.random.set_seed(13)
 
 
 def train_val_tf(x_train,
@@ -252,71 +252,73 @@ def data_pipe(df,
                 xtest=xtest, ytest=ytest)
 
 
+# def create_rolling_ts(
+#     input_data, 
+#     lookback=5, 
+#     return_target=True,
+#     apply_datefeatures=True,
+#     # return_array=False,
+#     **kwargs
+#     ):
+#     """
+#     Make flat data by using pd.concat instead, pd.concat([df1, df2]).
+#     Slow function.
+#     Save data as preprocessed?
+#     """
+#     x = []
+#     y = []
+#     rows = len(input_data)
+#     features = input_data.copy()
+#     target = input_data.copy()
+#     for i in range(rows - lookback):
+#         """Create embeddings for the date-features"""
+#         if apply_datefeatures:
+#             rolling_features = date_features(features.iloc[i: i + lookback])
+#         else:
+#             rolling_features = features.iloc[i: i + lookback]
 
-def create_rolling_ts(
-    input_data, 
-    lookback=5, 
-    return_target=True,
-    apply_datefeatures=True,
-    return_array=False,
-    **kwargs
-    ):
-    """
-    Make flat data by using pd.concat instead, pd.concat([df1, df2]).
-    Slow function.
-    Save data as preprocessed?
-    """
-    x = []
-    y = []
-    rows = len(input_data)
-    features = input_data.copy()
-    target = input_data.copy()
-    for i in range(rows - lookback):
-        """Create embeddings for the date-features"""
-        if apply_datefeatures:
-            rolling_features = date_features(features.iloc[i: i + lookback])
-        else:
-            rolling_features = features.iloc[i: i + lookback]
+#         rolling_target = target.iloc[i + lookback: i + lookback + 1]
+#         x.append(rolling_features)
+#         y.append(rolling_target)
+#     # if return_array:
+#     x = np.array(x)
+#     y = np.array(y)
 
-        rolling_target = target.iloc[i + lookback: i + lookback + 1]
-        x.append(rolling_features)
-        y.append(rolling_target)
-    if return_array:
-        x = np.array(x)
-        y = np.array(y)
-
-    if return_target:
-        return x, y
-    return x
-
-
-def split_data(data, train_size, valid_size):
-    """
-    Implement data based splitting. 
-    Do normalization.
-    
-    """
-    train_size = int(len(data) * train_size)
-    valid_size = int(train_size + len(data) * valid_size)
-    try:
-        train_set = data.iloc[: train_size]
-        valid_set = data.iloc[train_size: valid_size]
-        test_set = data.iloc[valid_size: ]
-        return train_set, valid_set, test_set
-    except Exception as e:
-        print(f'Exception from _split_data: {e}')
+#     if return_target:
+#         return x, y
+#     return x
 
 
-def flatten(data, num_cols=1):
-    return np.array(data).reshape(-1, num_cols)
+# def split_data(data, train_size, valid_size):
+#     """
+#     Implement data based splitting. 
+#     Do normalization.
+#     """
+#     train_size = int(len(data) * train_size)
+#     valid_size = int(train_size + len(data) * valid_size)
+#     try:
+#         train_set = data.iloc[: train_size]
+#         valid_set = data.iloc[train_size: valid_size]
+#         test_set = data.iloc[valid_size: ]
+#         return train_set, valid_set, test_set
+#     except Exception as e:
+#         print(f'Exception from _split_data: {e}')
 
 
-def date_features(df):
-    if isinstance(df, pd.core.series.Series):
-        df = pd.DataFrame(df, index=df.index)
+# def new_function():
+#     pass
 
-    df.loc[:, 'day_of_year'] = df.index.dayofyear
-    df.loc[:, 'month'] = df.index.month
-    df.loc[:, 'day_of_week'] = df.index.day
-    df.loc[:, 'hour'] = df.index.hour
-    return df
+
+# def factory():
+#     return True
+
+
+# def date_features(df):
+#     if isinstance(df, pd.core.series.Series):
+#         df = pd.DataFrame(df, index=df.index)
+
+#     df.loc[:, 'day_of_year'] = df.index.dayofyear
+#     df.loc[:, 'month'] = df.index.month
+#     df.loc[:, 'day_of_week'] = df.index.day
+#     df.loc[:, 'hour'] = df.index.hour
+#     return df
--- src/main.py
+++
@@ -1,7 +0,0 @@
-from loaders import loader
-
-
-if __name__ == "__main__":
-    data_loader = loader.TimeSeriesLoader(symbol="AAPL", interval=1)
-    df = data_loader.ts_intraday()
-    print(df.head())
