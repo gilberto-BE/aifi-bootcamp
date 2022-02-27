133ab9546a8788b4f4ddd7cd9141a1a5297a2d68
gilberto-BE <g.batres.estrada@gmail.com>
2022-02-27 19:23:26 +0100

    update

--- src/preprocess.py
+++ src/preprocess.py
@@ -76,3 +76,53 @@ def flatten(data):
 
 def is_pandas(df):
     return isinstance(df, (pd.core.frame.DataFrame, pd.core.series.Series))
+
+"""
+EXPERIMENTAL
+"""
+
+# transform a time series dataset into a supervised learning dataset
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols = list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+	# put it all together
+	agg = pd.concat(cols, axis=1)
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg.values
+ 
+# split a univariate dataset into train/test sets
+def train_test_split(data, n_test):
+	return data[:-n_test, :], data[-n_test:, :]
+
+ 
+# walk-forward validation for univariate data
+def walk_forward_validation(data, n_test):
+	predictions = list()
+	# split dataset
+	train, test = train_test_split(data, n_test)
+	# seed history with training dataset
+	history = [x for x in train]
+	# step over each time-step in the test set
+	for i in range(len(test)):
+		# split test row into input and output columns
+		testX, testy = test[i, :-1], test[i, -1]
+		# fit model on history and make a prediction
+		yhat = xgboost_forecast(history, testX)
+		# store forecast in list of predictions
+		predictions.append(yhat)
+		# add actual observation to history for the next loop
+		history.append(test[i])
+		# summarize progress
+		print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+	# estimate prediction error
+	error = mean_absolute_error(test[:, -1], predictions)
+	return error, test[:, -1], predictions
