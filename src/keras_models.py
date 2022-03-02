cb3a4df5b99d1cb80b9be0e317f43ce6b4b58183
gilberto-BE <g.batres.estrada@gmail.com>
2022-03-02 21:41:47 +0100

    update

--- src/keras_models.py
+++ src/keras_models.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 
+
 class KerasLSTM:
-    def __init__(self, ):
+    def __init__(self, input_shape=1, output_shape=1, learning_rate=0.03):
+        self.model = tf.keras.layers.Sequential()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.learning_rate = learning_rate
+
+    def create_model(self):
         pass
-    
